a126349d9f063efa9940c0f8852fa60b86080827
Zach Olauson <zmolauson@gmail.com>
2013-10-26 01:59:25 -0500

    add stories controller and views

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,12 +1,13 @@
 class StoriesController < ApplicationController
   def new
+  	@campaign = Campaign.find(params[:campaign_id])
     @hero = Hero.find(params[:hero_id])
     @story = @hero.stories.new
   end
 
   def create
     hero = Hero.find(params[:hero_id])
-    hero.stories.create(params[:story])
-    redirect_to campaign_hero_path(hero)
+    hero.stories.create(params[:story].permit(:body))
+    redirect_to campaign_hero_path(hero.campaign, hero)
   end
 end
