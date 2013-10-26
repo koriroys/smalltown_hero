ba83bf608c34c34990382315cea589bf99e3903e
Zach Olauson <zmolauson@gmail.com>
2013-10-25 22:24:22 -0500

    add stories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026023833) do
+ActiveRecord::Schema.define(version: 20131026030019) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,12 +24,21 @@ ActiveRecord::Schema.define(version: 20131026023833) do
     t.text     "description"
     t.integer  "prize_amount"
     t.string   "sponsoring_company"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-    create_table "heros", force: true do |t|
+  create_table "heros", force: true do |t|
     t.string   "name"
     t.integer  "campaign_id"
     t.integer  "like_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stories", force: true do |t|
+    t.text     "body"
+    t.integer  "hero_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
