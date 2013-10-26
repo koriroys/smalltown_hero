8846c1cd7ff6b7ffc447e70de24362f70ae7cb8d
Zach Olauson <zmolauson@gmail.com>
2013-10-26 01:58:48 -0500

    update heroes controller and add views

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -6,11 +6,18 @@ class HeroesController < ApplicationController
 
   def create
     campaign = Campaign.find(params[:campaign_id])
-    hero = campaign.heroes.create(params[:hero])
-    redirect_to new_campaign_hero_story_path(hero)
+    hero = campaign.heroes.create(params[:hero].permit(:name))
+    redirect_to new_campaign_hero_story_path(campaign, hero)
   end
 
   def show
-    @hero = Hero.find(params[:hero_id])
+    @hero = Hero.find(params[:id])
+  end
+
+  def update
+    @hero = Hero.find(params[:id])
+    @hero.like_count += 1 if params[:hero][:like_count]
+    @hero.save
+    redirect_to campaign_hero_path(@hero.campaign, @hero)
   end
 end
