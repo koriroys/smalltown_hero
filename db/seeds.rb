d5a5cdfac057e33d919c7e63537ec33cb467b007
Zach Olauson <zmolauson@gmail.com>
2013-10-25 22:24:45 -0500

    write seed data for campaign

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Campaign.create(
+  city: "Chicago",
+  state: "IL",
+  start_date:DateTime.now,
+  end_date:1.week.from_now,
+  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed posuere ante, eget porttitor sapien. Sed blandit orci eget felis rutrum, eu rutrum lectus imperdiet. Suspendisse laoreet urna quis mi lobortis facilisis. Nunc quis imperdiet neque. Cras eu dignissim nisi. Cras malesuada mauris a placerat tristique. Etiam laoreet eu magna quis gravida. Maecenas convallis dui odio, vitae facilisis mi vulputate nec. Aenean et justo sed libero malesuada blandit non non ligula.",
+  prize_amount:10000,
+  sponsoring_company:"coca-cola"
+)
+
