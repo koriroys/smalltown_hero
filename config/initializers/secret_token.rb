1ead6385f2730a9bef69ab4c06479600e2dfe529
Kori Roys <kori@koriroys.com>
2013-10-25 21:34:15 -0500

    initial commit - yes so original I know

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmalltownHero::Application.config.secret_key_base = 'd3c3481780cb5a49b16c63000348d842160dc3e6679aeacfbe09a8f82170a29d9d0964531c771d58c6e13dcc61f3ea5ff2612f0703582883c23016ca729a553d'
