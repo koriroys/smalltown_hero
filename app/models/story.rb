9995cdabe7d05705541515831bcb0bb1bae10da8
Zach Olauson <zmolauson@gmail.com>
2013-10-26 03:37:05 -0500

    add after create hook to increase hero like count on creation of story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,10 @@
 class Story < ActiveRecord::Base
   belongs_to :hero
+
+  after_create :increase_hero_like_count
+
+  def increase_hero_like_count
+    self.hero.like_count += 1
+    self.hero.save
+  end
 end
